ec3e4e2db56000a8f5388bf0ad726f4c8509d189
Tolga1999 <tolgadoganer@outlook.com>
2022-12-16 00:41:33 +0100

    made 10 buttons

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,6 @@
 // frontend button
 let frontendAnchor = document.querySelector('a:nth-of-type(1)');
-
 frontendAnchor.addEventListener('click', frontendAnchorClick);
-frontendAnchor.addEventListener('animationend', frontendAnchorClick);
 
 function frontendAnchorClick(){
   frontendAnchor.classList.toggle('frontend-animation');
@@ -10,30 +8,75 @@ function frontendAnchorClick(){
 
 // design button
 let designAnchor = document.querySelector('a:nth-of-type(2)');
-
 designAnchor.addEventListener('click', designAnchorClick);
-designAnchor.addEventListener('animationend', designAnchorClick);
 
 function designAnchorClick(){
-  designAnchor.classList.toggle('design-animation');
+  designAnchor.classList.add('design-animation');
 }
 
 // & button
 let andAnchor = document.querySelector('a:nth-of-type(3)');
-
 andAnchor.addEventListener('click', andAnchorClick);
-andAnchor.addEventListener('animationend', andAnchorClick);
 
 function andAnchorClick(){
-  andAnchor.classList.toggle('and-animation');
+  andAnchor.classList.add('and-animation');
 }
 
 // development button
 let developmentAnchor = document.querySelector('a:nth-of-type(4)');
-
 developmentAnchor.addEventListener('click', developmentAnchorClick);
-// developmentAnchor.addEventListener('animationend', developmentAnchorClick);
 
 function developmentAnchorClick(){
   developmentAnchor.classList.toggle('development-animation');
 }
+
+// Sprint 5 button
+let sprint5Anchor = document.querySelector('a:nth-of-type(5)');
+sprint5Anchor.addEventListener('click', sprint5AnchorClick);
+
+function sprint5AnchorClick(){
+  sprint5Anchor.classList.add('sprint5-animation');
+}
+
+// Fix button
+let fixAnchor = document.querySelector('a:nth-of-type(6)');
+fixAnchor.addEventListener('click', fixAnchorClick);
+
+function fixAnchorClick(){
+  fixAnchor.classList.add('fix-animation');
+}
+
+// The button
+let theAnchor = document.querySelector('a:nth-of-type(7)');
+theAnchor.addEventListener('click', theAnchorClick);
+
+function theAnchorClick(){
+  theAnchor.classList.add('the-animation');
+}
+
+// Flow button
+let flowAnchor = document.querySelector('a:nth-of-type(8)');
+flowAnchor.addEventListener('click', flowAnchorClick);
+
+function flowAnchorClick(){
+  flowAnchor.classList.add('flow-animation');
+}
+
+// User button
+let userAnchor = document.querySelector('a:nth-of-type(9)');
+userAnchor.addEventListener('click', userAnchorClick);
+
+function userAnchorClick(){
+  userAnchor.classList.add('user-animation');
+}
+
+// User button
+let interfaceAnchor = document.querySelector('a:nth-of-type(10)');
+interfaceAnchor.addEventListener('click', interfaceAnchorClick);
+
+function interfaceAnchorClick(){
+  interfaceAnchor.classList.add('interface-animation');
+}
+
+// // een animationend om het double click probleem op te lossen VAN JUSTUS (functie wordt weer aangeroepen nadat de animatie eindigt)
+// sprint5Anchor.addEventListener('animationend', sprint5AnchorClick);
